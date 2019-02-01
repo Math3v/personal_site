a9b2dfc7684f217bb8e98513e127f95087e5a7ed
Matej Minarik <mato.minarik@azet.sk>
2019-02-01 21:25:39 +0100

    Update Wallaby and Deps

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule PersonalSite.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:wallaby, "~> 0.20.0", [runtime: false, only: :test]},
+      {:wallaby, "~> 0.21.0", [runtime: false, only: :test]},
       {:guardian, "~> 1.0"},
       {:comeonin, "~> 4.0"},
       {:bcrypt_elixir, "~> 0.12"},

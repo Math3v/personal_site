c8df1c33d67461a5c83dbc48f425d4b8f6bbb499
Matej Minarik <mato.minarik@azet.sk>
2019-02-02 09:58:41 +0100

    Allow Tags Input as Comma-Delimited-String

--- lib/personal_site/blog/post.ex
+++ lib/personal_site/blog/post.ex
@@ -6,6 +6,7 @@ defmodule PersonalSite.Blog.Post do
     field :body, :string
     field :slug, :string
     field :tags, {:array, :string}
+    field(:tags_input, :string, virtual: true)
     field :title, :string
 
     timestamps()
@@ -14,7 +15,23 @@ defmodule PersonalSite.Blog.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :body, :tags, :slug])
-    |> validate_required([:title, :body, :tags, :slug])
+    |> cast(attrs, [:title, :body, :tags_input, :slug])
+    |> validate_required([:title, :body, :tags_input, :slug])
+    |> tags_input_to_tags_array()
+  end
+
+  defp tags_input_to_tags_array(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{tags_input: tags_input}} ->
+        tags =
+          tags_input
+          |> String.split(",")
+          |> Enum.map(&String.trim/1)
+
+        put_change(changeset, :tags, tags)
+
+      _ ->
+        changeset
+    end
   end
 end

55aa87e0addc9554a74339c6ae44c7e8e39c3971
Matej Minarik <mato.minarik@azet.sk>
2019-02-02 13:52:58 +0100

    Design Navbar

--- lib/personal_site_web/views/layout_view.ex
+++ lib/personal_site_web/views/layout_view.ex
@@ -3,6 +3,12 @@ defmodule PersonalSiteWeb.LayoutView do
 
   def signed_in?(conn), do: is_signed_in?(Guardian.Plug.current_resource(conn))
 
+  def has_flash?(conn, key) do
+    conn
+    |> get_flash(key)
+    |> is_binary()
+  end
+
   defp is_signed_in?(nil), do: false
   defp is_signed_in?(_), do: true
 end

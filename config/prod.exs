8d580a677e6abfeaea875ef38f090e15be29fab5
Matej Minarik <mato.minarik@azet.sk>
2019-02-08 08:08:11 +0100

    Disable SSL For Render

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :personal_site, PersonalSiteWeb.Endpoint,
 config :personal_site, PersonalSite.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: false # Needed for Render
 
 # ## SSL Support
 #

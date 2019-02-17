d9d3380e994d2ee728780004d4abdd9b81026d44
Matej Minárik <mato.minarik@azet.sk>
2019-02-17 09:44:39 +0100

    Fix Usability Issues (#19)
    * Show Published At
    
    * Hide Published At If Not Present
    
    * Fix Responsive Icons

--- lib/personal_site_web/views/post_view.ex
+++ lib/personal_site_web/views/post_view.ex
@@ -3,6 +3,13 @@ defmodule PersonalSiteWeb.PostView do
 
   def post_disabled?(post), do: is_post_disabled?(post.published_at)
 
+  def format_datetime(%DateTime{day: day, month: month, year: year}) do
+    "#{make_two_digit(day)}. #{make_two_digit(month)}. #{year}"
+  end
+
+  defp make_two_digit(number) when number < 10, do: "0#{number}"
+  defp make_two_digit(number), do: "#{number}"
+
   defp is_post_disabled?(published_at) when is_nil(published_at), do: true
   defp is_post_disabled?(_), do: false
 end
